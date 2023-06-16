579d23f082e6a0720e8db0d93aa0b7612261b072
Yancap <yangabriel3001f@gmail.com>
2023-06-16 13:20:58 -0300

    configurações iniciais

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <section>
+        <div>
+            <div>
+                <Image src='/facebook.svg' alt='Facebook' height={106} width={301}/>
+            </div>
+            <div></div>
+        </div>
+        <div>
+
+        </div>
+    </section>
+  )
+}
