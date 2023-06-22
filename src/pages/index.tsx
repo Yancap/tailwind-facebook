7a57e683b326a1922166fe4d1118674614ab66df
Yancap <yangabriel3001f@gmail.com>
2023-06-22 20:05:54 -0300

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         familiares e com as pessoas que você conhece." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://static.xx.fbcdn.net/rsrc.php/yb/r/hLRJ1GG_y0J.ico" />
+        <link rel="icon" href="./favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet"/>
       </Head>
       <main className='pt-10 w-full grid place-items-center desktop:pb-5 desktop:pt-0 desktop:min-h-screen desktop:pb-0 desktop:bg-main-gray'>
